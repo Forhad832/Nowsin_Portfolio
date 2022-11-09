e424922915dc1e9c42deb4db3839b6729b0f33f3
Forhad <forhadhossain0619@gmail.com>
2022-11-09 23:46:28 +0600

    change navbar logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,13 +2,25 @@ import React, { useState } from 'react';
 
 import { GoThreeBars } from 'react-icons/go';
 import { MdClose } from 'react-icons/md';
+import Logo from '../../assets/nafi.jpg';
 const Navbar = () => {
   const [nav,setNav] = useState(false);
   return (
-    <nav className='fixed top-0 z-50 w-full py-4 bg-white shadow h-14'>
+    <nav className='fixed top-0 z-50 w-full py-4 bg-white h-18'>
       <div className="container px-4 mx-auto">
         <div className="flex items-center justify-between">
-            <a href="home" className='text-lg font-semibold'>Nowsin Akter</a>
+            <a href="home" className='font-semibold'>
+              <div className="flex gap-4">
+              <div>
+                  <img src={Logo} alt="logo" className='object-contain w-12 h-12 rounded-full' />
+                </div>
+                <div>
+                <p className='text-lg'>Nowsin Akter</p>
+                <p className='text-sm'><small>Graphic Designer</small></p>
+                </div>
+              
+              </div>
+            </a>
             <ul className='items-center justify-around hidden gap-5 md:flex'>
                 <li><a className='text-lg font-semibold' href="#home">Home</a></li>
                 <li><a className='text-lg font-semibold' href="#about">About</a></li>
